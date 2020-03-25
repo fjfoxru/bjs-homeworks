2a1c97338928df51b0fd42482ba484f32e3f6739
Alexandra Lyashenko <fjfox@yandex.ru>
2020-03-25 22:42:20 +0300

    Задание 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,47 @@
+'use strict';
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let disc = b**2 - 4 * a * c;
+    let x = [];
+    if (disc < 0) {
+      return x;
+    } else if (disc == 0) {
+      return x[0] = (-b) / 2 * a;
+    } else {
+      x[0] = (-b - Math.sqrt(disc)) / 2 * a;
+      x[1] = (-b + Math.sqrt(disc)) / 2 * a;
+      return x;
+    }
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(){
+    let marks = [];
+    let countMarks = arguments.length;
+    let totalMarks;
+    for (let i = 0; i < arguments.length; i++) {
+    marks.push(arguments[i]);
+    }
+    
+    if (countMarks > 5) {
+      console.log('Оценок введено больше 5');
+      marks.splice(0, 4);
+      return totalMarks = (marks.reduce((a, b) => a + b)) / countMarks;
+    } else if (marks == '') {
+      return 0;
+    } else {
+      return totalMarks = (marks.reduce((a, b) => a + b)) / countMarks;
+    }
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+        
+    let dateNow = new Date();
+    dateNow = dateNow.getFullYear();
+    let userAge = dateNow - dateOfBirthday;
+
+    if (userAge > 18 ) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
 }
