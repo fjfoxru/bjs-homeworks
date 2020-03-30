ab085af02f8c0457c016fcf0f77d57cdf7cd0be8
Alexandra Lyashenko <fjfox@yandex.ru>
2020-03-30 22:52:08 +0300

    task fix

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -6,28 +6,27 @@ function getResult(a,b,c){
     if (disc < 0) {
       return x;
     } else if (disc == 0) {
-      return x[0] = (-b) / 2 * a;
+      x[0] = (-b) / 2 * a;
+      return x;
     } else {
-      x[0] = (-b - Math.sqrt(disc)) / 2 * a;
-      x[1] = (-b + Math.sqrt(disc)) / 2 * a;
+      x[0] = (-b + Math.sqrt(disc)) / 2 * a;
+      x[1] = (-b - Math.sqrt(disc)) / 2 * a;
       return x;
     }
 }
 
-function getAverageMark(){
-    let marks = [];
-    let countMarks = arguments.length;
-    let totalMarks;
-    for (let i = 0; i < arguments.length; i++) {
-    marks.push(arguments[i]);
+function getAverageMark(marks){
+    if (marks == '') {
+      return 0;
     }
-    
+    let countMarks = marks.length;
+    let totalMarks;
+
     if (countMarks > 5) {
       console.log('Оценок введено больше 5');
-      marks.splice(0, 4);
+      marks.splice(5);
+      countMarks = marks.length;
       return totalMarks = (marks.reduce((a, b) => a + b)) / countMarks;
-    } else if (marks == '') {
-      return 0;
     } else {
       return totalMarks = (marks.reduce((a, b) => a + b)) / countMarks;
     }
