6742bf8f9b069ec710192e08f71fcf25252868f9
Alexandra Lyashenko <fjfox@yandex.ru>
2020-03-31 14:02:26 +0300

    2 task fixed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,12 +1,20 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+        let dateNow = new Date();
+        let dateCredit = new Date(date);
+        let month = Math.round((dateCredit.getTime() - dateNow.getTime()) / (1000 * 3600 * 24 * 30));
+        let amountCredit = amount - contribution;
+        let monthlyPayment = amountCredit * ( (percent / 12) + (percent / 12) / ( ( (1 + percent / 12) ** month ) - 1));
+        let allsum = monthlyPayment * month;
+
+        if (isNaN(+percent) || isNaN(+contribution) || isNaN(+amount)) {
+            return `Параметр <название параметра> содержит неправильное значение <значение параметра>`;
+        } else {
+            return allsum.toFixed(2);
+        }
 }
 
 function getGreeting(name) {
-    if (name == '' || name == undefined ) {
-        name = 'Аноним';
-    }
-    console.log(`Привет, мир! Меня зовут ${name}`);
-    return `Привет, мир! Меня зовут ${name}`;
+    let isValid = (name == '' || name == undefined);
+    let message = `Привет, мир! Меня зовут ${isValid ? "Аноним" : name}`;
+    return message;
 }
