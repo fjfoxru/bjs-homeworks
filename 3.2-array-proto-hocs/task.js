101d4cd1a4a1962144b6f3a09bb5b4a2e69b09d9
Alexandra Lyashenko <fjfox@yandex.ru>
2020-04-15 12:41:33 +0300

    all tasks added

--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -6,44 +6,57 @@ function sleep(milliseconds)
 
 function sum(...args) {
     // Замедление на половину секунды.
-    sleep(500); // Можно использовать другое значение замедления.
+    sleep(50); // Можно использовать другое значение замедления.
     return args.reduce((sum, arg) => {
       return sum += +arg;
     }, 0);
   }
 
-
-
-
-
-
- const compareArrays = ( arr1, arr2 ) => arr1.every((arr, i) => arr === arr2[i] );
-
-
-
-
+ const compareArrays = ( arr1, arr2 ) => arr1.every((arr, i) => (arr === arr2[i] || arr.length == arr2.length));
 
  function memorize (fn, limit) {
-
   const memory = [];
-   
-  
-
   return function (...args) {
     let currentResult = {};
-    currentResult.result = fn(...args);
     currentResult.args = args;
     
-    let findInMemory = memory.find((item, index, array) => compareArrays(item.args, currentResult.args));
-    
-    if(findInMemory)
-    memory.push(currentResult);
-   }
+    let findInMemory = memory.find((item) => {
+      if(compareArrays(item.args, currentResult.args)) {
+        return item.result;
+      }});
 
+    if(!findInMemory) {
+      currentResult.result = fn(...args);
+      memory.push(currentResult);
+      if(memory.length > limit) {
+        memory.shift();
+      }
+    }
  }
+}
 
 
- const mSum = memorize(sum, 2);
- mSum(3, 4); // 7
- mSum(1, 3); // 4
- mSum(3, 4);
+function testCase (testFunction, timerName) {
+  let testArrays = [ [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] ];
+  console.time(timerName);
+  for(i = 0; i < 10; i++) {
+    testArrays.forEach(function (currentArray) {
+      testFunction(...currentArray);
+    })
+  }
+  console.timeEnd(timerName);
+}
+
+
+const mSum = memorize(sum, 3);
+mSum(3, 4); // 7
+mSum(1, 3); // 4
+mSum(3, 4);
+mSum(16, 4, 3);
+mSum(1, 3);
+
+
+testCase(mSum, 'Таймер новой функции суммы');
+testCase(sum, 'Таймер функции суммы');
+
+
